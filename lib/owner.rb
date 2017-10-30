901eeefab12ab296885d4d337819591c599b36f0
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-10-30 18:20:19 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,62 @@
 class Owner
   attr_accessor :name, :pets
-  attr_reader :
+  attr_reader :species
 
-  def initialize(name)
-    @name = name
-    @mood = "nervous"
+@@all = []
+
+  def initialize(species)
+    @pets = {fishes: [], cats: [], dogs: []}
+    @species = species@@all << self
+end
+
+def self.all
+  @@all
+end
+def self.reset_all
+  self.all.clear
+end 
+
+def self.count
+  self.all.size
+end
+
+def say_species
+  "I am a human."
+end
+def buy_fish(name)
+  @pets[:fishe] << Fish.new(name)
+end
+
+def buy_cat(name)
+  @pets[:cats] << Cat.new(name)
+end
+
+def buy_dog(name)
+  @pets[:dogs] << Dog.new(name)
+end
+
+def walk_dogs
+  @pets[:dogs].each { |dog| dog.mood = "happy" }
+end
+
+def play_with_cats
+    @pets[:cats].each { |cat| cat.mood = "happy" }
   end
+
+  def feed_fish
+    @pets[:fishes].each { |fish| fish.mood = "happy"}
   end
+
+  def sell_pets
+    @pets.each do |species, pets|
+      pets.ech do |pet|
+        pet.mood = "nervous"
+      end
+
+      pets.clear
+    end
+    def list_pets
+      "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dogs, and #{pets[:cats].size}cats."
+    end
+    end
+  
